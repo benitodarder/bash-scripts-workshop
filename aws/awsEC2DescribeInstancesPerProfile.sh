e51bcf07dc8ab0beeb0ec14a72290469b26c8f4e
benitodarder <benitodarder@gmail.com>
2024-06-04 21:52:15 +0200

    Removing Profile suffix.

--- aws/awsEC2DescribeInstancesPerProfile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-	echo "usage: "$0".sh <Profile>"
-	exit 1
-fi 
-set -x  
-
-aws ec2 describe-instances --query 'Reservations[].Instances[].[PrivateIpAddress,InstanceId,InstanceType,Tags[?Key==`Name`]| [0].Value,State.Name]' --output table --profile $1
